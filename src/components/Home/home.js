936ecb76c5cd6f4c48d429c2d3d740b204c0bb1a
Pradosh Thirunavukkarasu <tpradosh360@gmail.com>
2024-12-07 20:05:52 -0800

    top left and right mostly done

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,29 +2,68 @@ import React, {useState} from 'react';
 import './home.css';
 
 import pfpImage from '../images/bird.webp';
+import linkdin from '../images/linkdin.png';
+import github from '../images/git.png';
+import resume from '../images/resume.png';
 
 
 function Home (props){
     return(
-
         <div className = "main">
-            <div className ="top">
 
-                <div className = "pfp">
-                    <img src = {pfpImage} alt ="" class="pic"></img>
+            <div className = "top">
+                <div className = "top-left">
+                    <div className = "pfp-container">
+                        <img class = "pic circle" src = {pfpImage} alt=""></img>
+                    </div>
+                    <div className = "links">
+                        <img src = {linkdin} class = "linkdin icon"></img>
+                        <img src = {github} class = "git icon"></img>
+                        <img src = {resume} class = "resume icon"></img>
+
+                    </div>
                 </div>
 
-                <div className = "intro">
-                    <p>👋 Hi There </p>
-                    <p>Im Pradosh Thirunavukkarsu, a sophmore 
-                        @uci currently studying computer science
-                    </p>
+                <div className = "top-right">
+                    <h1 className = "emoji">👋</h1>
+                    <h1 class="typing-effect">
+                        <span>I</span>
+                        <span>'</span>
+                        <span>m</span>
+                        <span>&nbsp;</span>
+                        <span>P</span>
+                        <span>r</span>
+                        <span>a</span>
+                        <span>d</span>
+                        <span>o</span>
+                        <span>s</span>
+                        <span>h</span>
+                        <span>,</span><br></br>
+                        <span>W</span>
+                        <span>e</span>
+                        <span>l</span>
+                        <span>c</span>
+                        <span>o</span>
+                        <span>m</span>
+                        <span>e</span>
+                        <span>&nbsp;</span>
+                        <span>t</span>
+                        <span>o</span>
+                        <span>&nbsp;</span>
+                        <span>m</span>
+                        <span>y</span>
+                        <span>&nbsp;</span>
+                        <span>W</span>
+                        <span>e</span>
+                        <span>b</span>
+                        <span>s</span>
+                        <span>i</span>
+                        <span>t</span>
+                        <span>e</span>
+                        <span>!</span>
+                    </h1>
                 </div>
-
             </div>
-
-
-
 
         </div>
     )
